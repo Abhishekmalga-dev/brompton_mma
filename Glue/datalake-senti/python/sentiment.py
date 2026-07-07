ffe52635308f641122c689eaae24ec291a7a92f4
malgaabhishek29 <malgaabhishek29@gmail.com>
2026-07-07 15:37:20 -0500

    Update sentiment.py

--- Glue/datalake-senti/python/sentiment.py
+++ Glue/datalake-senti/python/sentiment.py
@@ -25,3 +25,41 @@ EVENT_DATE_PATH = "s3://psegli-datalakenonprodli-datalake-temp-dev/ccaas/event_d
 
 arrears_df = None
 ----
+
+
+# PATHS
+
+ACCOUNT_TIER = "nonprod" if ENV == "dev" else "prod"
+TEMP_BUCKET = f"psegli-datalake{ACCOUNT_TIER}li-datalake-temp-{ENV}"
+CURATED_BUCKET = f"psegli-datalake{ACCOUNT_TIER}li-datalake-curated-{ENV}"
+
+ARREARS_DATABASE = f"datalake-curated-{ENV}"
+
+EVENT_DATE_PATH = f"s3://{TEMP_BUCKET}/ccaas/event_dates/survey_api_json/"
+
+CURATED_IVR_PATH = f"s3://{CURATED_BUCKET}/ccaas/survey_customer_sat_ivr/"
+CURATED_TXN_PATH = f"s3://{CURATED_BUCKET}/ccaas/survey_sms_web_transactional/"
+CURATED_REL_PATH = f"s3://{CURATED_BUCKET}/ccaas/survey_sms_web_relational/"
+
+INTENT_MAPPING_PATH = f"s3://{TEMP_BUCKET}/sentiment_analysis/Intent_Mapping/"
+
+FINAL_IVR_STAGING = f"s3://{CURATED_BUCKET}/sentiment_analysis/staging/ivr/"
+FINAL_TXN_STAGING = f"s3://{CURATED_BUCKET}/sentiment_analysis/staging/txn/"
+FINAL_REL_STAGING = f"s3://{CURATED_BUCKET}/sentiment_analysis/staging/rel/"
+
+FINAL_IVR_PATH = f"s3://{CURATED_BUCKET}/sentiment_analysis/final/ivr/"
+FINAL_TXN_PATH = f"s3://{CURATED_BUCKET}/sentiment_analysis/final/transactional/"
+FINAL_REL_PATH = f"s3://{CURATED_BUCKET}/sentiment_analysis/final/relational/"
+
+AGG_IVR_FILE = f"s3://{CURATED_BUCKET}/sentiment_analysis/aggregate/ivr.parquet"
+AGG_TXN_FILE = f"s3://{CURATED_BUCKET}/sentiment_analysis/aggregate/txn.parquet"
+AGG_REL_FILE = f"s3://{CURATED_BUCKET}/sentiment_analysis/aggregate/rel.parquet"
+
+YTD_IVR_FILE = f"s3://{CURATED_BUCKET}/sentiment_analysis/ytd/ivr"
+YTD_TXN_FILE = f"s3://{CURATED_BUCKET}/sentiment_analysis/ytd/txn"
+YTD_REL_FILE = f"s3://{CURATED_BUCKET}/sentiment_analysis/ytd/rel"
+
+# temp folders for single-file writes
+AGG_IVR_TMP = f"s3://{CURATED_BUCKET}/sentiment_analysis/aggregate/tmp_ivr/"
+AGG_TXN_TMP = f"s3://{CURATED_BUCKET}/sentiment_analysis/aggregate/tmp_txn/"
+AGG_REL_TMP = f"s3://{CURATED_BUCKET}/sentiment_analysis/aggregate/tmp_rel/"
